568266025fa0b2d50c693944ae8502e68cdd94e5
m.umair.kn <m.umair.kn@gmail.com>
2026-06-23 21:01:57 +0500

    E: Added Player logic (cricleshape.py and player.py)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
 
@@ -13,18 +14,24 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for dr in drawable:
+            dr.draw(screen) 
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-
-
-
 
 if __name__ == "__main__":
     main()
